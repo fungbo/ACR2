a504f05c955aaa629627548804171c5407f4b2f9
bfeng <fungbo@126.com>
2016-11-12 00:18:03 +0800

    Add 'error' state

--- src/app/constants/FlowState.js
+++ src/app/constants/FlowState.js
@@ -2,3 +2,5 @@ export const IDLE = 'IDLE';
 export const WAIT_FOR_UUID = 'WAIT_FOR_UUID';
 export const WAIT_FOR_QR = 'WAIT_FOR_QR';
 export const FINISHED = 'FINISHED';
+
+export const ERROR = 'ERROR';
